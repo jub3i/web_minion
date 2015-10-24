18c3de7565845b0ae3984e0ccfcbd4725290536b
jub3i <anon66@gmail.com>
2015-10-24 20:44:05 +0200

    add in missing client side auth functions

---
+++ assets/public/auth.js
@@ -0,0 +1,40 @@
+//login function
+function login(username, password) {
+  $.ajax({
+    type: 'POST',
+    url: '/login',
+    async: true,
+    data: {
+      username: username,
+      password: password,
+    },
+  }).done(function(data) {
+    if (data.sc === 0) {
+      console.log(data);
+    } else {
+      alert('Login failed: incorrect username or password');
+    }
+  }).fail(function(res) {
+    alert('Login failed: network error');
+    console.err(res);
+  });
+}
+
+//logout function
+function logout() {
+  $.ajax({
+    type: 'POST',
+    url: '/logout',
+    async: true,
+  }).done(function(data) {
+    if (data.sc === 0) {
+      console.log(data);
+    } else {
+      alert('Logout failed');
+    }
+  }).fail(function(res) {
+    console.err(res);
+    alert('Logout failed');
+  });
+}
+
