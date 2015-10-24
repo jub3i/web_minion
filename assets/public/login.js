81ed3699a8373443284cdd21bdf09e4b1bdb7f59
jub3i <anon66@gmail.com>
2015-10-24 21:17:43 +0200

    implement cancel btn on client side login

--- assets/public/login.js
+++ assets/public/login.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $('#loginBtn').click(function() {
     login($('#unInput').val(), $('#pwInput').val());
   });
+
+  $('#cancelBtn').click(function() {
+    $('#unInput').val('');
+    $('#pwInput').val('');
+  });
 });
